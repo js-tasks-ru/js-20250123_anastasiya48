518bf73242ed4b2ff8503c991e67a71deb2f398d
Anastasia <127987021+NastasiaMat@users.noreply.github.com>
2025-01-26 23:28:16 +0300

    модуль 3, задача createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathArr = path.split('.');
 
+  const result = (obj) => {
+    let res = obj;
+
+    for (let i = 0; i < pathArr.length; i += 1) {
+      if (!res.hasOwnProperty(pathArr[i])) {
+        return undefined;
+      }
+      res = res[pathArr[i]];
+    }
+
+    return res;
+  }; 
+  return result;
 }
