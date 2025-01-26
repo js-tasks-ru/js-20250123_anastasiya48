7170ecaacbc3ac6cff989326705f6c8bcf25358d
Anastasia <127987021+NastasiaMat@users.noreply.github.com>
2025-01-26 16:00:46 +0300

    omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,9 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  const newObj = obj;
+  
+  fields.map((item) => delete newObj[item]);
 
+  return newObj;
 };
