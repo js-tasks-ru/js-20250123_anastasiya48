4a077b0aef2c379c4dfb9fdd5271fd6c69c47bb9
Anastasia <127987021+NastasiaMat@users.noreply.github.com>
2025-02-01 22:47:18 +0300

    trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,25 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
 
+  const stringArr = [];
+  const count = size ?? string.length;
+  let j = 0;
+
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i - 1] !== string[i]) {
+      stringArr.push(string[i]);
+      j = 1;
+      continue;
+    }
+    if (string[i - 1] === string[i] && j < count) {
+      stringArr.push(string[i]);
+      j += 1;
+    } 
+  }
+
+  return stringArr.join('');
 }
