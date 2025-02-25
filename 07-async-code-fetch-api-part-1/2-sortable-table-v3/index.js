f33b0966992f331ab6f6502ae4f5019cbb8de804
Anastasia <127987021+NastasiaMat@users.noreply.github.com>
2025-02-25 19:10:26 +0300

    fix: добавила скролл и исправила загрузку данных

--- 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
@@ -3,6 +3,11 @@ import SortableTableV2 from "../../06-events-practice/1-sortable-table-v2/index.
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
 export default class SortableTable extends SortableTableV2 {
+  startLoading = 0
+  endLoading = 30
+  stepLoading = 10
+  isLoading = false
+
   constructor(headersConfig, {
     url = '',
   } = {}) {
@@ -10,42 +15,76 @@ export default class SortableTable extends SortableTableV2 {
     this.url = url;
     this.isSortLocally = false;
 
-    this.render(this.createUrl());
+    this.render(this.createUrl(this.startLoading, this.endLoading));
   }
 
-  createUrl() {
+  createUrl(start, end) {
     const url = new URL(this.url, BACKEND_URL);
 
     url.searchParams.append('_sort', this.sortField);
     url.searchParams.append('_order', this.sortOder);
-    // url.searchParams.append('_start', this.sortOder);
-    // url.searchParams.append('_end', this.sortOder);
+    url.searchParams.append('_start', start);
+    url.searchParams.append('_end', end);
 
     return url.toString();
   }
 
-  // sort() {
-  //   this.render(this.createUrl());
-  // }
-
   async render(url) {
     try {
       const response = await fetchJson(url);
-      const data = response;
-      this.data = data;
+
+      if (this.isLoading) {
+        this.data = [...this.data, ...response];
+      } else {
+        this.data = response;
+      }
+
       super.updateProductsList();
+
     } catch (err) {
       console.log(err);
     }
   }
 
+  async handleWindowScroll() {
+    const documentHeight = document.body.clientHeight;
+    const windowHeight = window.innerHeight;
+    const scrolled = window.scrollY;
+    const shouldFetch = scrolled + windowHeight >= documentHeight - 200;
 
+    if (shouldFetch) {
+      this.isLoading = true;
 
-  sortOnClient () {
-    super.sort(this.sortField, this.sortOder);
+      this.startLoading += this.stepLoading;
+      this.endLoading += this.stepLoading;
+
+      await this.render(this.createUrl(this.startLoading, this.endLoading))
+    }
   }
 
-  sortOnServer () {
-    this.render(this.createUrl());
+  createListeners() {
+    super.createListeners();
+
+    this.handleWindowScroll = this.handleWindowScroll.bind(this);
+
+    window.addEventListener('scroll', this.handleWindowScroll);
+  }
+
+  destroyListeners() {
+    super.destroyListeners();
+    window.removeEventListener('scroll', this.handleWindowScroll);
+  }
+
+  async sortOnServer () {
+    this.isLoading = false;
+
+    this.startLoading = 0;
+    this.endLoading = this.stepLoading;
+
+    await this.render(this.createUrl(this.startLoading, this.endLoading));
+  }
+
+  sortOnClientrt() {
+    super.sort(this.sortField, this.sortOder);
   }
 }
